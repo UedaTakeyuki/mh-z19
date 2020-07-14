b7d50e82ede0ce1d39c1bd579df34d162923c2c2
Yu-Hao Chang <9203700@gmail.com>
2020-07-14 20:13:02 +0800

    Add utility module

---
+++ pypi/mh_z19/utils.py
@@ -0,0 +1,20 @@
+import getrpimodel
+import struct
+
+from pathlib import Path
+
+
+def compute_checksum(array: list):
+    return struct.pack('B', 0xff - (sum(array) % 0x100) + 1)
+
+
+def get_default_serial_device() -> str:
+    pi_model = getrpimodel.model()
+    pi_model_strict = getrpimodel.model_strict()
+
+    if Path('/dev/serial0').resolve().exists():
+        return '/dev/serial0'
+    elif pi_model == '3 Model B' or pi_model_strict == 'Zero W':
+        return '/dev/ttyS0'
+    else:
+        return '/dev/ttyAMA0'
