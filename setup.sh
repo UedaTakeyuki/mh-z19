743f81cac4f8811f850e79044b500a5d5ed5748b
Takeyuki UEDA <grenouill.vert@gmail.com>
2022-03-23 19:54:24 +0900

    bullseye support

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-sudo apt-get install python-pip git-core
-sudo pip install mh_z19 pondslider incremental_counter error_counter
+sudo apt-get install python3-pip git-core
+sudo pip3 install mh_z19 pondslider incremental_counter error_counter
 git clone https://github.com/UedaTakeyuki/handlers
 ln -s handlers/value/sender/send2monitor/send2monitor.py
 sudo sed -i "s/^enable_uart=.*/enable_uart=1/" /boot/config.txt
